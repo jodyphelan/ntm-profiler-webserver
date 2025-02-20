7700e6d71709a6a4960820f1ec1e0e9cd5749379
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-02-20 10:01:22 +0000

    update redis port

--- src/ntm_profiler_webserver/__init__.py
+++ src/ntm_profiler_webserver/__init__.py
@@ -36,8 +36,8 @@ def create_app(test_config=None):
 
     app.config.from_mapping(
           CELERY=dict(
-              broker_url= os.environ['REDIS_URL'] if 'REDIS_URL' in os.environ else 'redis://localhost:6379',
-              result_backend=os.environ['REDIS_URL'] if 'REDIS_URL' in os.environ else 'redis://localhost:6379',
+              broker_url= os.environ['REDIS_URL'] if 'REDIS_URL' in os.environ else 'redis://localhost:7777',
+              result_backend=os.environ['REDIS_URL'] if 'REDIS_URL' in os.environ else 'redis://localhost:7777',
           ),
     )
 
